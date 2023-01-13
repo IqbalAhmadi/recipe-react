23c5cab4295b3cfb689ec8f241ab636b7e69d114
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2023-01-12 16:37:48 -0800

    created the route and components, imported fonts to style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Pages from './pages/Pages'
+import Category from './components/Category'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Category />
       <Pages />
     </div>
   )
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Home from './Home'
+import {Route, Routes} from 'react-router-dom'
+import Cuisine from './Cuisine'
+
 
 function Pages() {
   return (
-    <div>
-        <Home/>
-    </div>
+    <Routes>
+        <Route path='/' element={<Home/>}/>
+    </Routes>
   )
 }
 
